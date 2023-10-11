362614e843d2bee3341d9a2fb8e9b267c29d776d
apply <apply@applys-MBP.fibertel.com.ar>
2023-10-11 17:00:35 -0300

    add support for offline navigation

--- hooks/useHitsData.ts
+++ hooks/useHitsData.ts
@@ -1,26 +1,55 @@
 import {useState, useEffect} from 'react';
 import {type Hit} from '../types';
+import NetInfo from '@react-native-community/netinfo';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function useHitsData() {
   const [hits, setHits] = useState<Hit[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    setLoading(true);
-    setError(null);
+  const [online, setOnline] = useState<boolean | null>(true);
 
-    fetch('https://hn.algolia.com/api/v1/search_by_date?query=mobile')
-      .then(async res => await res.json())
-      .then(res => {
-        setHits(res.hits);
-        setLoading(false);
-      })
-      .catch(err => {
-        setError(err);
-        setLoading(false);
+  useEffect(() => {
+    const checkNetworkStatus = () => {
+      NetInfo.fetch().then(state => {
+        setOnline(state.isConnected);
       });
-  }, []);
+    };
+
+    checkNetworkStatus();
+
+    NetInfo.addEventListener(state => {
+      setOnline(state.isConnected);
+    });
+
+    if (!online) {
+      AsyncStorage.getItem('cachedData')
+        .then(data => {
+          if (data) {
+            setHits(JSON.parse(data));
+          }
+        })
+        .catch(error => {
+          setError(error);
+        });
+    } else {
+      setLoading(true);
+      setError(null);
+
+      fetch('https://hn.algolia.com/api/v1/search_by_date?query=mobile')
+        .then(async res => await res.json())
+        .then(res => {
+          setHits(res.hits);
+          setLoading(false);
+          return AsyncStorage.setItem('cachedData', JSON.stringify(res.hits));
+        })
+        .catch(err => {
+          setError(err);
+          setLoading(false);
+        });
+    }
+  }, [online]);
 
   return {hits, loading, error};
 }
